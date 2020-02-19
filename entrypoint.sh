9c270106a1893e90e82a953f30a8430fa6774288
Dairyo <sekinedairyou@gmail.com>
2020-02-20 00:26:59 +0900

    enabled rmecab

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ if [ -v REPOS ]; then
     git clone $REPOS /root/.jupyter/lab/user-settings/@jupyterlab
 fi
 cd /sync > /dev/null 2>&1
-jupyter trust /sync/*.ipynb
+jupyter trust /sync/*.ipynb > /dev/null 2>&1
 jupyter lab --port 8888 --no-browser --ip=0.0.0.0 --allow-root
--- init.R
+++ init.R
@@ -1,5 +1,6 @@
 repos = "https://cran.ism.ac.jp/"
 install.packages("tidyverse", repos=repos)
+install.packages ("RMeCab", repos = "http://rmecab.jp/R", type = "source")
 install.packages('IRkernel', repos=repos)
 IRkernel::installspec()
 IRkernel::installspec(user = FALSE)
